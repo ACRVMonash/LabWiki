f63ab558b615afc0b50791e2656b28f5a597b789
Timo Stoffregen <timo.stoffregen@outlook.com>
2019-02-14 15:30:34 +1100

    Added setup script for Docker container

---
+++ Wiki Resources/Scripts/setup.sh
@@ -0,0 +1,14 @@
+pip install gym tqdm jupyter_contrib_nbextensions
+pip install gym[atari]
+pip install pyvirtualdisplay
+apt-get update
+apt-get install xvfb python-opengl -y
+apt install libzmqpp-dev libeigen3-dev libopencv-dev -y
+jupyter contrib nbextension install --user
+jupyter nbextension enable collapsible_headings/main
+jupyter nbextension enable varInspector/main
+jupyter nbextension enable toc2/main
+
+jupyter notebook --no-browser --ip=0.0.0.0 --port=8887 --allow-root --NotebookApp.token= --notebook-dir='/storage'
+
+
