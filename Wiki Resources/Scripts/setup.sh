0253be89802c28d431c40e2fc36d23cb8c2e25c3
TimoStoff <40829734+TimoStoff@users.noreply.github.com>
2019-02-14 16:06:02 +1100

    Update setup.sh

--- Wiki Resources/Scripts/setup.sh
+++ Wiki Resources/Scripts/setup.sh
@@ -1,14 +1,10 @@
-pip install gym tqdm jupyter_contrib_nbextensions
-pip install gym[atari]
-pip install pyvirtualdisplay
+pip install tqdm jupyter_contrib_nbextensions
 apt-get update
 apt-get install xvfb python-opengl -y
-apt install libzmqpp-dev libeigen3-dev libopencv-dev -y
+apt install libeigen3-dev libopencv-dev -y
 jupyter contrib nbextension install --user
 jupyter nbextension enable collapsible_headings/main
 jupyter nbextension enable varInspector/main
 jupyter nbextension enable toc2/main
 
 jupyter notebook --no-browser --ip=0.0.0.0 --port=8887 --allow-root --NotebookApp.token= --notebook-dir='/storage'
-
-
