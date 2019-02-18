eba09bb8a4b91fa572a0e58757a5e5acdca0d920
ACRVMonash <47617814+ACRVMonash@users.noreply.github.com>
2019-02-19 10:52:06 +1100

    Update setup.sh

--- Wiki Resources/Scripts/setup.sh
+++ Wiki Resources/Scripts/setup.sh
@@ -1,10 +1,17 @@
+#Pip
+pip install --upgrade pip
 pip install tqdm jupyter_contrib_nbextensions
-apt-get update
-apt-get install xvfb python-opengl -y
-apt install libeigen3-dev libopencv-dev -y
+pip install --upgrade pytorch #Update pytorch
+
+#OpenAI Gym
+#pip install gym
+#pip install gym[atari]
+
+#Set up jupyter
 jupyter contrib nbextension install --user
 jupyter nbextension enable collapsible_headings/main
 jupyter nbextension enable varInspector/main
 jupyter nbextension enable toc2/main
 
-jupyter notebook --no-browser --ip=0.0.0.0 --port=8887 --allow-root --NotebookApp.token= --notebook-dir='/storage' &
+#Run jupyter
+jupyter notebook --no-browser --ip=0.0.0.0 --port=8887 --allow-root --NotebookApp.token= --notebook-dir='/storage'
